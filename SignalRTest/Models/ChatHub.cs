d9c4e1ed3b229ade763f79cfc020fcdb82d4f762
mahuwei <mahuwei@msn.com>
2019-02-11 14:59:58 +0800

    添加项目文件。

---
+++ SignalRTest/Models/ChatHub.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRTest.Models {
+    public class ChatHub : Hub {
+        public async Task NewMessage(string username, string message) {
+            await Clients.All.SendAsync("messageReceived", username, message);
+        }
+    }
+}
---
+++ SignalRTest/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SignalRTest.Models;
+
+namespace SignalRTest {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseSignalR(options => { options.MapHub<ChatHub>("/hub"); });
+
+            app.UseCors(builder =>
+                builder.WithOrigins(Configuration["AllowedHosts"])
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+
+            app.UseMvc();
+        }
+    }
+}
